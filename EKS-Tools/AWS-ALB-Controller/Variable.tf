7d4ed0804d7801925ab13a73ddbc5a335fe006e7
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2024-03-28 16:22:49 +0200

    Create Variable.tf

---
+++ EKS-Tools/AWS-ALB-Controller/Variable.tf
@@ -0,0 +1,23 @@
+variable "eks_issuer" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "alb_sa_name" {
+  type = string
+}
+
+variable "alb_policy_name" {
+  type = string
+}
+
+variable "alb_role_name" {
+  type = string
+}
+
+variable "values_file" {
+  type = string
+}
