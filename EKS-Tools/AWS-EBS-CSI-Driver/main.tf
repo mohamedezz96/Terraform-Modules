ad5ad26d644a7d6c00979acd67f1bea33c900232
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2024-04-17 17:08:37 +0200

    Create main.tf

---
+++ EKS-Tools/AWS-EBS-CSI-Driver/main.tf
@@ -0,0 +1,30 @@
+resource "helm_release" "aws_ebs_csi_driver" {
+  name       = "aws-ebs-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver/"
+  chart      = "aws-ebs-csi-driver"
+  version    = var.aws_ebs_csi_driver_version
+  namespace        = "aws-ebs-csi-driver"
+  create_namespace = true
+  wait             = false
+  values = ["${file(var.values_yaml)}"]
+
+  set {
+    name  = "controller.serviceAccount.name"
+    value = var.aws_ebs_csi_driver_controller_sa_name
+  }
+
+  set {
+    name  = "node.serviceAccount.name"
+    value = var.aws_ebs_csi_driver_node_sa_name
+  }
+
+  set {
+    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = data.aws_iam_role.aws_ebs_csi_driver_role.arn
+  }
+
+  set {
+    name  = "node.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = data.aws_iam_role.aws_ebs_csi_driver_role.arn
+  }
+}
