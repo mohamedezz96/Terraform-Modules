bf35a44de6f0f1b0628a40969709a62728c07739
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2024-04-18 12:15:33 +0200

    Update variable.tf

--- EKS-Tools/AWS-EBS-CSI-Driver/variable.tf
+++ EKS-Tools/AWS-EBS-CSI-Driver/variable.tf
@@ -15,16 +15,3 @@ variable "values_yaml" {
   description = "The path to the values.yaml file for the AWS EBS CSI driver Helm chart."
   
 }
-
-variable "aws_ebs_csi_driver_controller_sa_name" {
-  type        = string
-  description = "The name of the AWS EFS CSI driver service account."
-  
-}
-
-
-variable "aws_ebs_csi_driver_node_sa_name" {
-  type        = string
-  description = "The name of the AWS EFS CSI driver node service account."
-  
-}
