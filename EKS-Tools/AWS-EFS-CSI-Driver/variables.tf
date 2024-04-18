e14293254ba64fffa9632b270c7c73e5bef355a9
mohamedezz96 <100701600+mohamedezz96@users.noreply.github.com>
2024-04-18 14:38:17 +0200

    Update variables.tf

--- EKS-Tools/AWS-EFS-CSI-Driver/variables.tf
+++ EKS-Tools/AWS-EFS-CSI-Driver/variables.tf
@@ -1,25 +1,15 @@
 variable "aws_efs_csi_driver_version" {
-  type = string
+  type        = string
+  description = "The version of the AWS EFS CSI driver Helm chart to install."
 }
 
 variable "eks_issuer" {
-  type = string
-}
-
-
-variable "aws_efs_controller_sa_name" {
-  type = string
-}
-
-variable "aws_efs_node_sa_name" {
-  type = string
-}
-
-variable "aws_efs_csi_driver_role_name" {
-  type = string
+  type        = string
+  description = "The OIDC issuer URL for the EKS cluster."
 }
 
 
 variable "values_file" {
-  type = string
+  type        = string
+  description = "The path to the values.yaml file for the AWS EFS CSI driver Helm chart."
 }
