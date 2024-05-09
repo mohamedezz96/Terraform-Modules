4147ab645e73b239bd3a403ff5c4e4ce81873511
mohamed ezz <mohamed.elsayedezzeldin@vodafone.com>
2024-05-09 12:30:55 +0300

    editing network for eks

--- EKS/Network/eks_network.tf
+++ EKS/Network/eks_network.tf
@@ -87,24 +87,11 @@ resource "aws_nat_gateway" "eks-nat-gateway" {
 resource "aws_route_table" "private-subnet-route-table" {
   vpc_id = aws_vpc.eks_vpc.id
 
-  route = [
-    {
+  route {
       cidr_block                 = "0.0.0.0/0"
       nat_gateway_id             = aws_nat_gateway.eks-nat-gateway.id
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      gateway_id                 = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      core_network_arn           = ""
-      vpc_peering_connection_id  = ""
-    },
-  ]
+    }
+  
 
   tags = {
     Name = "eks-private-subnet-route-table"
@@ -114,24 +101,10 @@ resource "aws_route_table" "private-subnet-route-table" {
 resource "aws_route_table" "public-subnet-route-table" {
   vpc_id = aws_vpc.eks_vpc.id
 
-  route = [
-    {
+  route {
       cidr_block                 = "0.0.0.0/0"
       gateway_id                 = aws_internet_gateway.eks_vpc_igw.id
-      nat_gateway_id             = ""
-      carrier_gateway_id         = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id     = ""
-      instance_id                = ""
-      ipv6_cidr_block            = ""
-      local_gateway_id           = ""
-      network_interface_id       = ""
-      transit_gateway_id         = ""
-      vpc_endpoint_id            = ""
-      core_network_arn           = ""
-      vpc_peering_connection_id  = ""
-    },
-  ]
+    }
 
   tags = {
     Name = "eks-public-subnet-route-table"
