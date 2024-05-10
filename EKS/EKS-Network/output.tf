a425513e56ca8a3dfd23d3a9e3c26fe27cd7c6b4
mohamed ezz <mohamed.elsayedezzeldin@vodafone.com>
2024-05-10 14:02:08 +0300

    adding cluster

--- EKS/EKS-Network/output.tf
+++ EKS/EKS-Network/output.tf
@@ -1,10 +1,14 @@
 output "eks_vpc_private_subnet_ids" {
-  value = aws_subnet.eks_vpc_private_subnet[*].id
+  value = [for subnet_key, subnet_value in aws_subnet.eks_vpc_private_subnet : subnet_value.id]
 }
+
 output "eks_vpc_public_subnet_ids" {
   value = [for subnet_key, subnet_value in aws_subnet.eks_vpc_public_subnet : subnet_value.id]
 }
 
 output "eks_vpc_subnet_ids" {
-  value = concat(aws_subnet.eks_vpc_private_subnet[*].id, aws_subnet.eks_vpc_public_subnet[*].id)
+  value = concat(
+    [for subnet_key, subnet_value in aws_subnet.eks_vpc_public_subnet : subnet_value.id],
+    [for subnet_key, subnet_value in aws_subnet.eks_vpc_private_subnet : subnet_value.id]
+  )
 }
