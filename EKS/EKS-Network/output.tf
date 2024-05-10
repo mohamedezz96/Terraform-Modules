2c6d4bbb3faef2c4389525d23273d378131a571c
mohamed ezz <mohamed.elsayedezzeldin@vodafone.com>
2024-05-10 13:59:02 +0300

    adding cluster

--- EKS/EKS-Network/output.tf
+++ EKS/EKS-Network/output.tf
@@ -2,7 +2,7 @@ output "eks_vpc_private_subnet_ids" {
   value = aws_subnet.eks_vpc_private_subnet[*].id
 }
 output "eks_vpc_public_subnet_ids" {
-  value = aws_subnet.eks_vpc_public_subnet[*].id
+  value = [for subnet_key, subnet_value in aws_subnet.eks_vpc_public_subnet : subnet_value.id]
 }
 
 output "eks_vpc_subnet_ids" {
