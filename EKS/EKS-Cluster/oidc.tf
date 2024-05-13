8a291e40c69d1c38f674a05ea20c859fb43cb8fc
mohamed ezz <mohamed.elsayedezzeldin@vodafone.com>
2024-05-13 16:23:46 +0300

    adding oidc

--- EKS/EKS-Cluster/oidc.tf
+++ EKS/EKS-Cluster/oidc.tf
@@ -2,9 +2,9 @@ data "tls_certificate" "eks_cluster_tls_cert" {
   url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
 }
 
-# resource "aws_iam_openid_connect_provider" "eks_cluster_oidc_provider" {
-#   client_id_list  = ["sts.amazonaws.com"]
-#   thumbprint_list = [data.tls_certificate.eks_cluster_tls_cert.certificates[0].sha1_fingerprint]
-#   url             = data.tls_certificate.eks_cluster_tls_cert.url
-#   depends_on = [aws_eks_cluster.eks_cluster]  
-# }
+resource "aws_iam_openid_connect_provider" "eks_cluster_oidc_provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks_cluster_tls_cert.certificates[0].sha1_fingerprint]
+  url             = data.tls_certificate.eks_cluster_tls_cert.url
+  depends_on = [aws_eks_cluster.eks_cluster]  
+}
