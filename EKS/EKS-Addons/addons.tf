62c0903f95120c277c0905cff8059f916f5dfeec
mohamed ezz <mohamed.elsayedezzeldin@vodafone.com>
2024-05-13 10:33:34 +0300

    adding eks addons

---
+++ EKS/EKS-Addons/addons.tf
@@ -0,0 +1,23 @@
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name = var.cluster_name
+  addon_name   = "vpc-cni"
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name = var.cluster_name
+  addon_name   = "kube-proxy"
+}
+
+resource "aws_eks_addon" "core_dns" {
+  cluster_name = var.cluster_name
+  addon_name   = "coredns"
+}
+
+
+
+
+
+
+
+
+
