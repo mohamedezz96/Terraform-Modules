3afc08a164a28b30b6adf1a8f4e925df4584efa7
mohamed ezz <mohamed.elsayedezzeldin@vodafone.com>
2024-05-15 23:21:02 +0300

    adding admin role

--- EKS-Tools/Cluster-Access-Management/policy.tf
+++ EKS-Tools/Cluster-Access-Management/policy.tf
@@ -55,7 +55,7 @@ resource "null_resource" "configmap-edit" {
       #!/bin/bash
       rolearn="${aws_iam_role.eks_admin_role.arn}"  
       file="${path.module}/maproles.txt"  
-      sed -i "/username: eks-admin/{N;s/username: eks-admin\n/username: eks-admin\n  rolearn: \"$rolearn\"\n/}" $file
+      sed -i "/username: eks-admin/{N;s/username: eks-admin/username: eks-admin\n  rolearn: $rolearn/}" "$file"
     EOT
   }
 }
